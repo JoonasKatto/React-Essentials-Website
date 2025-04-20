bf60fbb0779421fc00b6dc6df55fb85305653fc1
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-04-20 13:37:47 +0300

    Refactor Header component to display a random description from the reactDescriptions array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+// This function is used to get a random integer between 0 and max (inclusive)
+// It is used to select a random description from the reactDescriptions array
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
-  return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      Fundamental React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>);
+
+  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
