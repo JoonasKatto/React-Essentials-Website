3aeaa821ec7239dcad319eac4524e7e09cdfdeef
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-04-20 19:23:58 +0300

    Refactor App component to utilize separate Header and CoreConcept components for improved organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-// This function is used to get a random integer between 0 and max (inclusive)
-// It is used to select a random description from the reactDescriptions array
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={description} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import reactImg from '../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+// This function is used to get a random integer between 0 and max (inclusive)
+// It is used to select a random description from the reactDescriptions array
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+
+    const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+  
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
