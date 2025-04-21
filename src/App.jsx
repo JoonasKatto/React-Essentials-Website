1aeb66b0cd6d5fe5d1452d90cdf75d7afb9c14c0
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-04-21 21:47:26 +0300

    Add EXAMPLES data structure and update App component to display React examples

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState('Click a button to see the content');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -33,7 +34,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
