69243866f8d5c5936c1df7f54f741dbfe39ec158
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-04-21 18:28:48 +0300

    Add onSelect prop to TabButton component for improved interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("you clicked a tab");
+  }
+
   return (
     <div>
       <Header />
@@ -20,11 +24,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,7 @@
-export default function TabButton({ children }) {
-
-    function handleClick() {
-        console.log(`You clicked on ${children}`);
-    }
-
+export default function TabButton({ children, onSelect }) {
     return (
         <li>
-            <button onClick={handleClick}>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     );
 }
