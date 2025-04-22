307e32981b3f47be72f5c87e6a57e0b2b960c6f4
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-04-22 15:33:49 +0300

    Refactor Tabs component to allow customizable button container and improve structure in Examples

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,9 +1,9 @@
 // Wrapper component to provide structure for the Examples section of the website
-export default function Tabs({children, buttons}) {
+export default function Tabs({children, buttons, ButtonsContainer = 'menu'}) {
     return <>
-        <menu>
+        <ButtonsContainer>
             {buttons}
-        </menu>
+        </ButtonsContainer>
         {children}
     </>
 }
